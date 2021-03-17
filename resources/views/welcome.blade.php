afdff2583843eafc55e9be2f90e3af7787cc1555
athira-gopi <athugopi1999@gmail.com>
2021-03-17 11:57:33 +0530

    message

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col col-12 col-sm-3 col-lg-3 col-xl-3">
+<div class="col col-12 col-sm-3 col-lg-3 col-xl-3">
+
+<h1><center>Registration</center></h1>
+    <table class="table">
+    <tr>
+        <td>Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>RollNo</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Regno</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>College</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    </table>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
